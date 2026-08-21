0dc215a3d27c0d8ca006672ee2c019d6985a5790
Yuri Kuzmichev <kuzmichev@it-tian.ru>
2026-08-21 12:06:31 +0500

    refactor: упрощение функции processGlobule и замена стримов на синхронные операции

--- gulp/tasks/processGlobule.js
+++ gulp/tasks/processGlobule.js
@@ -1,41 +1,30 @@
-import gulp from "gulp";
-import through2 from "through2";
 import fs from "fs";
 import path from "path";
+import { globSync } from "glob";
 
-export const processGlobule = () => {
-  let mixins = "";
-
-  return gulp
-    .src("src/html/blocks/libs/**/_*.pug", {
+export const processGlobule = (done) => {
+  try {
+    const mixinFiles = globSync("src/html/blocks/libs/**/_*.pug", {
       ignore: "src/html/blocks/libs/_libs.pug",
-    })
-    .pipe(
-      through2.obj((file, _, cb) => {
-        // Получаем имя файла и добавляем в контент миксинов
-        const fileName = path.basename(file.path);
-        mixins += `include ${fileName}\n`;
-        cb(null, file);
-      })
-    )
-    .on("end", () => {
-      const filePath = "src/html/blocks/libs/_libs.pug";
-
-      // Читаем текущее содержимое файла
-      fs.readFile(filePath, "utf8", (err, currentContent) => {
-        if (err && err.code !== "ENOENT") throw err;
-
-        // Если контент не изменился, не перезаписываем файл
-        if (currentContent === mixins) {
-          console.log("Mixins have not changed.");
-          return;
-        }
-
-        // Записываем новый контент только если он отличается
-        fs.writeFile(filePath, mixins, (err) => {
-          if (err) throw err;
-          console.log("Mixins are generated automatically!");
-        });
-      });
     });
+    const mixins = mixinFiles
+      .map((filePath) => `include ${path.basename(filePath)}\n`)
+      .join("");
+    const filePath = "src/html/blocks/libs/_libs.pug";
+    const currentContent = fs.existsSync(filePath)
+      ? fs.readFileSync(filePath, "utf8")
+      : "";
+
+    if (currentContent === mixins) {
+      console.log("Mixins have not changed.");
+      done();
+      return;
+    }
+
+    fs.writeFileSync(filePath, mixins, "utf8");
+    console.log("Mixins are generated automatically!");
+    done();
+  } catch (error) {
+    done(error);
+  }
 };
